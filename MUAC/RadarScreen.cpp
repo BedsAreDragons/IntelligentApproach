390e95ae926a242966e080ba4ef048e6278a5382
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2026-01-21 18:26:51 +0000

    Changed to seconds

--- MUAC/RadarScreen.cpp
+++ MUAC/RadarScreen.cpp
@@ -496,7 +496,7 @@ void RadarScreen::OnRefresh(HDC hDC, int Phase)
 			double distance = minutesrequired * (147.5 / 3600.0);
 
 			double technicalrequired = landTime + lineupTime+departTime+20;
-			double technicaldistance = technicalrequired *(147.5 / 3600.0); 
+			double technicaldistance = technicalrequired *(147.5 / 3600.0);
 			double heavy = technicalrequired + 2.4;
 			double heavydistance = heavy * (147.5 /3600.0);
 
